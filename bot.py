16bdd182c0b7eeb53184433131fedaf4ad3d08dc
Gabriel047012 <gabrielandrade120705@gmail.com>
2026-07-31 18:07:00 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,6 +46,7 @@ async def planos(update: Update, context: ContextTypes.DEFAULT_TYPE):
         """📋PLANO MENSAL - R$ 4,99 Apenas
 
 😈Tenha acesso a uma grande variedade de conteúdos por menos de 5 reais por mês!
+
 📸🔥PARA VER ALGUMAS PRÉVIAS DIGITE:
 /previas"""
     )
